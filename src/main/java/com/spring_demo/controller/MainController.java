e122bdd16b0e59be665d47d8af30b6874451232f
mouseDH <mouse_dai@163.com>
2017-09-10 22:48:58 +0800

    模块4

--- src/main/java/com/spring_demo/controller/MainController.java
+++ src/main/java/com/spring_demo/controller/MainController.java
@@ -1,19 +1,84 @@
 package com.spring_demo.controller;
 
+import com.spring_demo.model.UserEntity;
+import com.spring_demo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.List;
 
 /**
  * Created by hank on 17-9-10.
  */
 @Controller
 public class MainController {
-
+    @Autowired
+    UserRepository userRepository;
 
     @RequestMapping(value = "/",method = RequestMethod.GET)
     public String index(){
         return "index";
+    }
+
+
+    @RequestMapping(value="/admin/users",method = RequestMethod.GET)
+    public String getUsers(ModelMap modelMap){
+        List<UserEntity> userList = userRepository.findAll();
+        modelMap.addAttribute("userList",userList);
+        return "admin/users";
+    }
+
+
+    @RequestMapping(value = "",method = RequestMethod.GET)
+    public String addUser(){
+
+        return "admin/user";
+    }
+
+
+    @RequestMapping(value = "/admin/users/addP",method = RequestMethod.POST)
+    public String addUserPost(@ModelAttribute("user") UserEntity userEntity){
+        userRepository.saveAndFlush(userEntity);
+        return "redirect:/admin/users";
+    }
+
+    // 更新用户信息 页面
+    @RequestMapping(value = "/admin/users/update/{id}", method = RequestMethod.GET)
+    public String updateUser(@PathVariable("id") Integer userId, ModelMap modelMap) {
+
+        // 找到userId所表示的用户
+        UserEntity userEntity = userRepository.findOne(userId);
+
+        // 传递给请求页面
+        modelMap.addAttribute("user", userEntity);
+        return "admin/updateUser";
+    }
+
+    // 更新用户信息 操作
+    @RequestMapping(value = "/admin/users/updateP", method = RequestMethod.POST)
+    public String updateUserPost(@ModelAttribute("userP") UserEntity user) {
+
+        // 更新用户信息
+        userRepository.updateUser(user.getNickname(), user.getFirstName(),
+                user.getLastName(), user.getPassword(), user.getId());
+        userRepository.flush(); // 刷新缓冲区
+        return "redirect:/admin/users";
+    }
+
+    // 删除用户
+    @RequestMapping(value = "/admin/users/delete/{id}", method = RequestMethod.GET)
+    public String deleteUser(@PathVariable("id") Integer userId) {
+
+        // 删除id为userId的用户
+        userRepository.delete(userId);
+        // 立即刷新
+        userRepository.flush();
+        return "redirect:/admin/users";
     }
 
 }
